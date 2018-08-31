637cfb067af48fdc453a635147ac863af2f5794f
Veronica Bustabad <vbustabad@gmail.com>
2018-08-31 02:41:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
 const currentUser = 'Grace Hopper';
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser}`;
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
